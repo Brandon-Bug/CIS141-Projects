f832ebefa8b91f90ddb75b54fc77bf539d5c3b95
Brandon Bugary <brandoneduacc@gmail.com>
2025-01-26 13:40:08 -0800

    Create skills_demonstration.py

---
+++ Programs/Module3/skills_demonstration.py
@@ -0,0 +1,8 @@
+main_string = "Code never lies, comments sometimes do"
+
+print(len(main_string))
+print(main_string.replace(", ",",\n"))
+print(main_string.upper())
+
+split_string = main_string[17:]
+print(split_string + " " + str(len(split_string)))
